3e37bc92d3015ea05048115620b5677458b1cb5d
nai-kon <fxtch686@yahoo.co.jp>
2023-12-15 20:09:36 +0900

    change jquery ajax to fetch api

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,25 @@
-
-
-let cvsIn = document.getElementById("inputimg");
-let ctxIn = cvsIn.getContext("2d");
-let divOut = document.getElementById("pred");
+const cvsIn = document.getElementById("inputimg");
+const ctxIn = cvsIn.getContext("2d");
+const divOut = document.getElementById("pred");
 let svgGraph = null;
 let mouselbtn = false;
 
-
 // initilize
-window.onload = ()=>{
-    
+window.onload = () => {    
     ctxIn.fillStyle = "white";
     ctxIn.fillRect(0, 0, cvsIn.width, cvsIn.height);
     ctxIn.lineWidth = 7;
-    ctxIn.lineCap = "round";
-    
+    ctxIn.lineCap = "round";    
     initProbGraph();
 }
 
 function initProbGraph(){
-
     const dummyData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]; // dummy data for initialize graph
-    const margin = { top: 10, right: 10, bottom: 10, left: 20 }
-        , width = 250, height = 196;
+    const margin = { top: 10, right: 10, bottom: 10, left: 20 };
+    const width = 250;
+    const height = 196;
 
-    let yScale = d3.scaleLinear()
+    const yScale = d3.scaleLinear()
         .domain([9, 0])
         .range([height, 0]);
     
@@ -43,20 +38,19 @@ function initProbGraph(){
         .data(dummyData)
         .enter()
         .append("rect")
-        .attr("y", (d,i)=>(yScale(i) - barHeight / 2))
+        .attr("y", (d,i) => (yScale(i) - barHeight / 2))
         .attr("height", barHeight)
         .style("fill", "green")
         .attr("x", 0)
-        .attr("width", (d)=>d * 2)
+        .attr("width", d => d * 2)
         .call(d3.axisLeft(yScale));
 }
 
-cvsIn.addEventListener("mousedown", (e)=>{ 
-
+cvsIn.addEventListener("mousedown", e => { 
     if(e.button == 0){
-        let rect = e.target.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
+        const rect = e.target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
         mouselbtn = true;
         ctxIn.beginPath();
         ctxIn.moveTo(x, y);
@@ -66,51 +60,51 @@ cvsIn.addEventListener("mousedown", (e)=>{
     }
 });
 
-cvsIn.addEventListener("mouseup", (e)=>{ 
+cvsIn.addEventListener("mouseup", e => { 
     if(e.button == 0){
         mouselbtn = false; 
         onRecognition();
     }
 });
 
-cvsIn.addEventListener("mousemove", (e)=>{
-    let rect = e.target.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let y = e.clientY - rect.top;
+cvsIn.addEventListener("mousemove", e => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
     if(mouselbtn){
         ctxIn.lineTo(x, y);
         ctxIn.stroke();
     }
 });
 
-cvsIn.addEventListener("touchstart", (e)=>{
+cvsIn.addEventListener("touchstart", e => {
     // for touch device
     if (e.targetTouches.length == 1) {
-        let rect = e.target.getBoundingClientRect();
-        let touch = e.targetTouches[0];
-        let x = touch.clientX - rect.left;
-        let y = touch.clientY - rect.top;
+        const rect = e.target.getBoundingClientRect();
+        const touch = e.targetTouches[0];
+        const x = touch.clientX - rect.left;
+        const y = touch.clientY - rect.top;
         ctxIn.beginPath();
         ctxIn.moveTo(x, y);
     }
 });
 
-cvsIn.addEventListener("touchmove", (e)=>{
+cvsIn.addEventListener("touchmove", e => {
     // for touch device
     if (e.targetTouches.length == 1) {
-        let rect = e.target.getBoundingClientRect();
-        let touch = e.targetTouches[0];
-        let x = touch.clientX - rect.left;
-        let y = touch.clientY - rect.top;
+        const rect = e.target.getBoundingClientRect();
+        const touch = e.targetTouches[0];
+        const x = touch.clientX - rect.left;
+        const y = touch.clientY - rect.top;
         ctxIn.lineTo(x, y);
         ctxIn.stroke();
         e.preventDefault();
     }
 });
 
-cvsIn.addEventListener("touchend", (e)=>onRecognition());
+cvsIn.addEventListener("touchend", e => onRecognition());
 
-cvsIn.addEventListener("contextmenu", (e)=>e.preventDefault());
+cvsIn.addEventListener("contextmenu", e => e.preventDefault());
 
 document.getElementById("clearbtn").onclick = onClear;
 function onClear(){
@@ -124,21 +118,22 @@ function onClear(){
 function onRecognition() {
     console.time("time");
 
-    cvsIn.toBlob((blob)=>{
-        let form = new FormData();
-        form.append('img', blob, "dummy.png")
-
-        $.ajax({
-            url: "./DigitRecognition",
-            type: "POST",
-            data: form,
-            processData: false,
-            contentType: false,
+    cvsIn.toBlob(blob => {
+        const body = new FormData();
+        body.append('img', blob, "dummy.png")
+        
+        fetch("./DigitRecognition", {
+            method: "POST",
+            body: body,
         })
-        .then(
-            (data)=>showResult(JSON.parse(data)),
-            ()=>alert("error")
-        )    
+        .then(res => {
+            if(!res.ok){
+                throw new Error("Network response was not OK");
+            }
+            return res.json();
+        })
+        .then(json => showResult(json))
+        .catch((error => alert("error", error)));
     })
 
     console.timeEnd("time");
@@ -157,5 +152,5 @@ function showResult(res){
         .transition()
         .duration(300)
         .style("fill", (d, i) => i == res.pred ? "blue":"green")
-        .attr("width", (d) => d * 2)
+        .attr("width", d => d * 2)
 }
